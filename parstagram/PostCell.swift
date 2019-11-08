629895c30c6bc79e97c334c4d0a8b704746f6aac
Angela Wen <wena@seas.upenn.edu>
2019-11-07 18:09:57 -0800

    Complete scrolling view of last 20 posts

---
+++ parstagram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  parstagram
+//
+//  Created by Angela Wen on 11/7/19.
+//  Copyright © 2019 Angela Wen. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
